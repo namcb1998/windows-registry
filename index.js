9f463f1340a3a4979e3a6d575682937430284eba
namcb1998 <namcb1998@gmail.com>
2021-03-04 13:54:36 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    registry: require('./lib/registry'),
+    error: require('./lib/error'),
+    types: require('./lib/types'),
+    windef: require('./lib/windef'),
+    Key: require('./lib/key'),
+    utils: require('./lib/utils')
+};
